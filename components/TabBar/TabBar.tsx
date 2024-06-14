a51f84aed4ce2d5199d7dcdca1b2b8ed10e9f338
Mhatw <contactomhatw@gmail.com>
2024-06-14 14:53:19 -0500

    add navbar

---
+++ components/TabBar/TabBar.tsx
@@ -0,0 +1,100 @@
+import { View, TouchableOpacity, StyleSheet, Text } from "react-native";
+import React from "react";
+import { BottomTabBarProps } from "@react-navigation/bottom-tabs";
+
+const TabBar = (props: BottomTabBarProps) => {
+  const { state, descriptors, navigation } = props;
+
+  return (
+    <View style={styles.tabBar}>
+      {state.routes.map((route, index) => {
+        const { options } = descriptors[route.key];
+        const label =
+          options.tabBarLabel !== undefined
+            ? options.tabBarLabel
+            : options.title !== undefined
+            ? options.title
+            : route.name;
+
+        if (["_sitemap", "+not-found"].includes(route.name)) return null;
+
+        const isFocused = state.index === index;
+        const focusStyle = isFocused
+          ? styles.tabBarTextFocused
+          : styles.tabBarText;
+
+        const onPress = () => {
+          const event = navigation.emit({
+            type: "tabPress",
+            target: route.key,
+            canPreventDefault: true,
+          });
+
+          if (!isFocused && !event.defaultPrevented) {
+            navigation.navigate(route.name, route.params);
+          }
+        };
+
+        const onLongPress = () => {
+          navigation.emit({
+            type: "tabLongPress",
+            target: route.key,
+          });
+        };
+
+        return (
+          <TouchableOpacity
+            key={route.name}
+            style={styles.tabBarItem}
+            accessibilityRole="button"
+            accessibilityState={isFocused ? { selected: true } : {}}
+            accessibilityLabel={options.tabBarAccessibilityLabel}
+            testID={options.tabBarTestID}
+            onPress={onPress}
+            onLongPress={onLongPress}
+          >
+            <Text style={focusStyle}>{`${label}`}</Text>
+          </TouchableOpacity>
+        );
+      })}
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  tabBar: {
+    position: "absolute",
+    bottom: 0,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    backgroundColor: "white",
+    paddingTop: 32,
+    paddingBottom: 40,
+    borderTopLeftRadius: 40,
+    borderTopRightRadius: 40,
+    borderCurve: "continuous",
+    shadowColor: "black",
+    shadowOffset: { width: 0, height: 0 },
+    shadowRadius: 10,
+    shadowOpacity: 1,
+    elevation: 10,
+    borderWidth: 1,
+    borderColor: "rgba(0,0,0,0.05)",
+  },
+  tabBarText: {
+    color: "black",
+  },
+  tabBarTextFocused: {
+    color: "#E6002A",
+  },
+  tabBarItem: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    color: "black",
+    gap: 4,
+  },
+});
+
+export default TabBar;
---
+++ components/TabBar/TabBar.types.ts
@@ -0,0 +1,7 @@
+export interface TabRoutes {
+  index: undefined;
+  two: undefined;
+  profile: undefined;
+}
+
+export type TabRoutesNames = keyof TabRoutes;
