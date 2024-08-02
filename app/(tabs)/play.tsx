5a45a66e584ae197d9985ecdd5ee01cd1c323b36
Esteban <esteban.diaz@trade.ec>
2024-08-02 15:49:34 -0500

    new 6

--- app/(tabs)/play.tsx
+++ app/(tabs)/play.tsx
@@ -1,20 +1,30 @@
 import { StyleSheet, TouchableOpacity, Image } from "react-native";
 
 import { Text, View } from "@/components/Themed";
+import { useFetchCategories } from "@/services/finance.services.hooks";
+import { useMemo } from "react";
+import { router } from "expo-router";
 
 export default function ProfileScreen() {
+  const { data } = useFetchCategories();
+  const ids = useMemo(() => {
+    if (!data) return [];
+    return data?.map((item) => item.id);
+  }, [data]);
+
+  const playRandom = () => {
+    const randomIndex = Math.floor(Math.random() * ids.length);
+    const id = ids[randomIndex];
+    router.push(`/category/${id}`);
+  };
+
   return (
     <View style={styles.container}>
       <Image
         style={styles.image}
         source={require("../../assets/images/play.gif")}
       />
-      <TouchableOpacity
-        style={styles.button}
-        onPress={() => {
-          console.log("play");
-        }}
-      >
+      <TouchableOpacity style={styles.button} onPress={playRandom}>
         <Text
           style={{
             color: "#fff",
